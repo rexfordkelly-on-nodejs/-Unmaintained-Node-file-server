6c6ab23c4d9219965a57ef0332075897a9a9b875
Weltschmerz <chlavois@gmail.com>
2012-10-11 14:41:31 +0000

    Update example

--- examples/express/server.js
+++ examples/express/server.js
@@ -5,17 +5,15 @@ var app = express.createServer()
 var port = 8080
 
 var lactate = require('lactate').Lactate({
-/*
-    cache:true,
-    expires:'one day'
-*/
+  //cache:false,
+  //max_age:'one day'
 })
 
 app.get('/', function(req, res) {
-    lactate.serve('index.html', req, res)
+  lactate.serve('index.html', req, res)
 })
 
 app.listen(port, function() {
-    console.log('Listening on port', port)
+  console.log('Listening on port', port)
 })
 
